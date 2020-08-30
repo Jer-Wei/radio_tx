8d701b23236d466367fc863fb710918bd3099862
Jer-Wei Rau <Jer-Wei@users.noreply.github.com>
2020-08-30 09:57:09 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,19 @@
+let Direction2 = 0
+let magnetic = 0
 let temperature = 0
 basic.showString("Radio TX")
 radio.setGroup(200)
-serial.redirectToUSB()
+input.calibrateCompass()
 basic.forever(function () {
     temperature = input.temperature()
-    radio.sendValue("temperature", temperature)
-    basic.showString("" + (temperature))
-    basic.pause(1000)
+    radio.sendValue("Temp", temperature)
+    led.toggle(0, 0)
+    basic.pause(100)
+    magnetic = input.magneticForce(Dimension.Strength)
+    radio.sendValue("Mag", magnetic)
+    basic.pause(100)
+    led.toggle(2, 2)
+    Direction2 = input.compassHeading()
+    radio.sendValue("Dir", Direction2)
+    basic.pause(100)
 })
